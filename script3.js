21911361d2ddcee8cfb5a33f44de21e1c4a936e8
HARISH-YADAV483 <harish4nitkkr@gmail.com>
2025-10-07 15:15:17 +0530

    Add files via upload

--- script3.js
+++ script3.js
@@ -1,17 +1,13 @@
-function showDivsByClassFromPlaceholder(inputId) {
-    // Get the input element by ID
-    var input = document.getElementById(inputId);
-    // Get the class name from the placeholder
-    var targetClass = input.placeholder.trim();
 
-    // Get all divs on the page
-    var divs = document.querySelectorAll('div');
-    divs.forEach(function(div) {
-        // Check if the div contains the target class
-        if (div.classList.contains(targetClass)) {
-            div.style.display = 'block';
-        } else {
-            div.style.display = 'none';
-        }
-    });
-}
+  var i = 0;
+  var text = "This platform is made for the welfare of the students of NIT Kurukshetra and the students who are willing to join nit kkr.This platform will provide to you all the information regarding this institute from basic to every important notification is shared by the student themselves.In the gossip coener you can chat with the students of nit kkr about any query or any doubt ,you can post your blogs and images and any other information regarding the institute.Enjoy this make platform your skills and share it with your friends and other relatives.";
+  var speed = 35; 
+
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("text").innerHTML += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+  typeWriter();
